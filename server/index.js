64cea99f28b41c55489e75e1cb4d62fe1692a3f0
wafuwafu13 <mariobaske@i.softbank.jp>
2020-09-03 12:36:42 +0900

    7 Simple WebSocket Application

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ const s = new server({ port: 5001 })
 s.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log("Received: " + message);
-
-        if (message == "hello") {
-            ws.send("hey there from the server!")
-        }
+        ws.send("From Server: " + message);
     })
 })
