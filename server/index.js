8cd1f9876d94338eeb6011ec68a15a446630f1e7
wafuwafu13 <mariobaske@i.softbank.jp>
2020-09-03 12:25:13 +0900

    Receiving Message on Server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
 const server = require('ws').Server;
 const s = new server({ port: 5001 })
+
+s.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log("Received: " + message);
+
+        if (message == "hello") {
+            ws.send("hey there from the server!")
+        }
+    })
+})
